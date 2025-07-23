68b5a4868a36d7dc79f5d5d18cc8b7e99bf3c61b
Nikhil Nambiar Chengat <nikhilnambiar1212@gmail.com>
2025-07-23 17:00:22 -0400

    learning how to map components
    added data file to pull and learn react and rendering array components

--- props-practice/src/App.jsx
+++ props-practice/src/App.jsx
@@ -1,27 +1,16 @@
-/**
- * Challenge: create a page that displays your favorite jokes
- * - Create a Joke component in its own file.
- * - Import and render 4-5 <Joke /> components
- * - Each Joke should receive a "setup" prop and a "punchline" prop
- *   and render those however you'd like
- * - Use your favorite 2-part jokes (setup & punchline), or check
- *   jokes.md file for some examples.
- *
- * EXTRA CREDIT:
- * Some jokes are only a punchline with no setup:
- *
- * E.g.: "It’s hard to explain puns to kleptomaniacs because
- * they always take things literally."
- *
- * If you don't pass in a "question" prop, how might you make it only
- * show the punchline?
- */
-
 import Joke from "./components/Joke";
 
 export default function App() {
-  return (
-    <>
+  const ninjarTurtles = [
+    <h2>Donatello</h2>,
+    <h2>Michaelangelo</h2>,
+    <h2>Rafael</h2>,
+    <h2>Leonardo</h2>,
+  ];
+  return <main>{ninjarTurtles}</main>;
+}
+{
+  /* <>
       <Joke punchline="It's hard to explain puns to kleptomaniacs because they always take things literally." />
       <Joke
         setup="I got my daughter a fridge for her birthday."
@@ -43,6 +32,5 @@ export default function App() {
         setup="What's the best thing about Switzerland?"
         punchline="I don't know, but the flag is a big plus!"
       />
-    </>
-  );
+    </> */
 }
